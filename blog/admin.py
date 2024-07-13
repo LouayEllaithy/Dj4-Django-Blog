aac8cf73721c3acd6a5c2383c04d391d44a8e30f
Louay Tawfik <louay.casper@gmail.com>
2024-07-13 21:10:46 +0300

    add summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
+
+# Apply summernote to all TextField in model.
+class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from blog.views import PostList, post_detail, PostCreate, PostUpdate, PostDelete
@@ -26,7 +26,8 @@ urlpatterns = [
     path('blog/', PostList.as_view()),
     path('blog/<int:pk>', post_detail),
     path('blog/<int:pk>/edit', PostUpdate.as_view()),
-    path('blog/<int:pk>/delete', PostDelete.as_view())
+    path('blog/<int:pk>/delete', PostDelete.as_view()),
+    path('summernote/', include('django_summernote.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
