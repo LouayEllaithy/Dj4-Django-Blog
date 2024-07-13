ea1db9b5b1f087d7aeb88d781d3bcb02e6420477
Louay Tawfik <louay.casper@gmail.com>
2024-07-13 21:42:20 +0300

    customize admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,16 @@ from .models import Post, Comment
 # Apply summernote to all TextField in model.
 class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
+    list_display = ['title', 'draft', 'author']
+    list_filter = ['draft', 'author']
+    search_fields = ['title', 'content']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created_at']
+    list_filter = ['user']
 
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
